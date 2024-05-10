ebe192c6b5cbaa03c527ad61ea70699ab0feb38e
sergio-nunez <sergio.nunez@laposte.net>
2024-05-10 11:12:02 +0200

    Add fonctions to handle connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,7 @@ export const allRoutes = [
     // new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La Galerie", "/pages/gallery.html"),
     // new Route("/gallery", "La Galerie", "/pages/gallery.html"),
-    new Route("/connexion", "Connexion", "/pages/auth/signin.html"),
+    new Route("/connexion", "Connexion", "/pages/auth/signin.html", "/js/auth/signin.js"),
     new Route("/inscription", "Inscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/compte", "Mon compte", "/pages/auth/account.html"),
     new Route("/motdepasse", "Mot de passe", "/pages/auth/editPassword.html"),
